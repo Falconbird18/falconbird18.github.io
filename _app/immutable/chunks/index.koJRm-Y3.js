76ac89270163d59082e65ce1f81a768b1f95b86c
Austin Tallent <austintallent25@outlook.com>
2024-06-01 21:35:17 -0700

    Initial commit.

--- _app/immutable/chunks/index.koJRm-Y3.js
+++
@@ -1,1 +0,0 @@
-import{i as n}from"./scheduler.D1cTx_it.js";function r(t,{delay:o=0,duration:e=400,easing:i=n}={}){const a=+getComputedStyle(t).opacity;return{delay:o,duration:e,easing:i,css:c=>`opacity: ${c*a}`}}export{r as f};
